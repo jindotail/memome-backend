b7c2ba73bd819e6e1956baf00ec9e7939fff762e
jindo.min <jindo.min@kakaocorp.com>
2022-05-21 16:54:18 +0900

    Chore: 파일 경로 변경
    - test/ -> test/e2e

--- tests/e2e/comment.test.ts
+++ tests/e2e/comment.test.ts
@@ -1,6 +1,6 @@
 import { default as request } from "supertest";
 import express from "express";
-import loaders from "../src/loaders";
+import loaders from "../../src/loaders";
 
 const app = express();
 
