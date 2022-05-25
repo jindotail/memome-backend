f44700908f2201a183b5a6a966c8bab852b63dc8
mimseong <parkseongmin98@gmail.com>
2022-05-25 17:52:56 +0900

    Chore: userIdx 가져오는 부분 함수로 분리
    - 같은 코드가 두 번 중복돼서 분리함

--- src/api/routes/comment.ts
+++ src/api/routes/comment.ts
@@ -9,6 +9,13 @@ const route = Router();
 export default (app: Router) => {
   app.use("/comment", route);
 
+  const getUserIdxById = async (userId: string): Promise<number> => {
+    const userModelInstance = Container.get(UserModel);
+    const user = await userModelInstance.findById(userId);
+
+    return user[0].idx;
+  };
+
   route.post(
     "/:userId",
     celebrate({
@@ -17,14 +24,10 @@ export default (app: Router) => {
       }),
     }),
     async (req: Request, res: Response) => {
-      const userModelInstance = Container.get(UserModel);
-      const user = await userModelInstance.findById(
-        req.params.userId as string
-      );
-
+      const userIdx = await getUserIdxById(req.params.userId as string);
       const commentServiceInstance = Container.get(CommentService);
       const result = await commentServiceInstance.create(
-        user[0].idx as number,
+        userIdx,
         req.body.comment
       );
       return res.status(201).json({
@@ -34,12 +37,9 @@ export default (app: Router) => {
   );
 
   route.get("/:userId", async (req: Request, res: Response) => {
-    const userModelInstance = Container.get(UserModel);
-    const user = await userModelInstance.findById(req.params.userId as string);
+    const userIdx = await getUserIdxById(req.params.userId as string);
     const commentServiceInstance = Container.get(CommentService);
-    const result = await commentServiceInstance.getComments(
-      user[0].idx as number
-    );
+    const result = await commentServiceInstance.getComments(userIdx);
     return res.status(200).json({
       comments: result,
     });
