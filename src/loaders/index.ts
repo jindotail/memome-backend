3acf5a183054435f3d9e4967074da3ee956b7bf7
mimseong <parkseongmin98@gmail.com>
2022-05-31 08:52:31 +0900

    Setting: dev 환경에서는 Mock Model 사용하도록 변경

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,22 +1,39 @@
 import CommentModel from "../models/comment";
 import UserModel from "../models/user";
+import MockCommentModel from "../models/mock_comment";
+import MockUserModel from "../models/mock_user";
 import dependencyInjectorLoader from "./dependencyInjector";
 import expressLoader from "./express";
 import Logger from "./logger";
+import config from "@/config";
+
+const getModelList = (
+  userModel: any,
+  commentModel: any
+): { name: string; model: any }[] => {
+  return [
+    {
+      name: "userModel",
+      model: userModel,
+    },
+    {
+      name: "commentModel",
+      model: commentModel,
+    },
+  ];
+};
 
 export default async ({ expressApp }) => {
-  const userModel = {
-    name: "userModel",
-    model: new UserModel(),
-  };
-  const commentModel = {
-    name: "commentModel",
-    model: new CommentModel(),
-  };
+  const modelList =
+    config.node_env === "dev"
+      ? getModelList(new MockUserModel(), new MockCommentModel())
+      : getModelList(new UserModel(), new CommentModel());
 
   dependencyInjectorLoader({
-    models: [userModel, commentModel],
+    models: modelList,
   });
+
+  Logger.info(`✌️ NODE_ENV: ${config.node_env}`);
   Logger.info("✌️ Dependency Injector loaded");
 
   expressLoader({ app: expressApp });
