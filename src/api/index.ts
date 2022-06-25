ec4e5622bdc1777bc085992d7c574bdb412b1f5b
mimseong <parkseongmin98@gmail.com>
2022-06-25 21:15:02 +0900

    Feat: logout 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import auth from "./routes/auth";
 import comment from "./routes/comment";
 import helloworld from "./routes/test";
+import user from "./routes/user";
 
 export default () => {
   const app = Router();
   comment(app);
   auth(app);
   helloworld(app);
+  user(app);
 
   return app;
 };
