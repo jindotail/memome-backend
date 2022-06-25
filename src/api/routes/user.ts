55c26a3f8f8a73ae382c76f200bf32b01b87c258
mimseong <parkseongmin98@gmail.com>
2022-06-25 21:38:25 +0900

    Feat: user 정보 들고오는 api 추가

---
+++ src/api/routes/user.ts
@@ -0,0 +1,31 @@
+import UserService from "@/services/user";
+import { NextFunction, Request, Response, Router } from "express";
+import { Container } from "typedi";
+import { Logger } from "winston";
+import middlewares from "../middlewares";
+
+const route = Router();
+
+export default (app: Router) => {
+  app.use("/user", route);
+  app.use("/user", middlewares.withError);
+
+  const logger: Logger = Container.get("logger");
+  const userServiceInstance = Container.get(UserService);
+
+  route.get(
+    "/:id",
+    async (req: Request, res: Response, next: NextFunction) => {
+      logger.debug(`Calling get /user/${req.params.id} endpoint`);
+
+      try {
+        const user = await userServiceInstance.getUserInfo(req.params.id as string);
+        return res.status(200).json({
+          nickname: user.nickname,
+        });
+      } catch (err) {
+        return next(err);
+      }
+    }
+  );
+};
--- src/services/user.ts
+++ src/services/user.ts
@@ -23,4 +23,17 @@ export default class UserService {
       );
     return user[0]?.idx;
   }
+
+  public async getUserInfo(userId: string): Promise<{ nickname: string }> {
+    this.logger.silly(`[UserService] getUserInfo ${userId}`);
+
+    const user = await this.UserModel.findById(userId);
+    if (user[0]?.idx === undefined)
+      throw new APIError(
+        "UserService",
+        HttpStatusCode.BAD_REQUEST,
+        "user not found"
+      );
+    return { nickname: user[0].nickname };
+  }
 }
