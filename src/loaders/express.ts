0ac6343fea83cecf9d3717445abe0916d97462c5
mimseong <parkseongmin98@gmail.com>
2022-07-12 16:06:58 +0900

    Setting: cors 설정 변경

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -13,7 +13,7 @@ export default ({ app }: { app: express.Application }) => {
   });
 
   app.use(bodyParser.json());
-  app.use(cors());
+  app.use(cors({ credentials: true, origin: "http://localhost:3000/" }));
   app.use(express.json());
   app.use(config.api.prefix, routes());
 
