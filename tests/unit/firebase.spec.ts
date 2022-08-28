33cd9de3d23c9580b55853c51e37ea627c242bb1
mimseong <parkseongmin98@gmail.com>
2022-08-28 19:24:08 +0900

    Test: firebase 접근 함수 test 추가

---
+++ tests/unit/firebase.spec.ts
@@ -0,0 +1,91 @@
+// import "reflect-metadata";
+import * as db from "../../src/models/firebase";
+
+beforeAll(async () => {});
+
+describe("auth", () => {
+  const id1 = "id1";
+  const id2 = "id2";
+  const name1 = "name1";
+  const name2 = "name2";
+
+  beforeEach(async () => {});
+
+  test("delete collection", async () => {
+    const collection = "test delete collection";
+    await db.saveDocument(collection, {});
+    await db.saveDocument(collection, {});
+
+    await db.deleteCollection(collection);
+
+    const result = await db.findCollection(collection);
+
+    expect(result.length).toEqual(0);
+  });
+
+  test("delete document", async () => {
+    const collection = "test delete document";
+    await db.deleteCollection(collection);
+
+    await db.saveDocumentWithId(collection, id1, { name: name1 });
+    await db.saveDocumentWithId(collection, id2, { name: name2 });
+
+    await db.deleteDocument(collection, id1);
+
+    const result = await db.findCollection(collection);
+
+    expect(result[0].id).toEqual(id2);
+    expect(result[0].data.name).toEqual(name2);
+  });
+
+  test("update document with id", async () => {
+    const collection = "test update document";
+    await db.deleteCollection(collection);
+
+    await db.saveDocumentWithId(collection, id1, { name: name1 });
+    await db.updateDocument(collection, id1, { name: name2 });
+  });
+
+  test("find collection", async () => {
+    const collection = "test find collection";
+    await db.deleteCollection(collection);
+
+    await db.saveDocumentWithId(collection, id1, { name: name1 });
+    await db.saveDocumentWithId(collection, id2, { name: name2 });
+
+    const result = await db.findCollection(collection);
+
+    expect(result[0].id).toEqual(id1);
+    expect(result[0].data.name).toEqual(name1);
+    expect(result[1].id).toEqual(id2);
+    expect(result[1].data.name).toEqual(name2);
+  });
+
+  test("find collection with condition", async () => {
+    const collection = "test find collection with condition";
+    await db.deleteCollection(collection);
+
+    await db.saveDocumentWithId(collection, id1, { name: name1 });
+    await db.saveDocumentWithId(collection, id2, { name: name2 });
+
+    const result = await db.findCollectionWithCondition(collection, {
+      fieldPath: "name",
+      opStr: "==",
+      value: name2,
+    });
+
+    expect(result[0].id).toEqual(id2);
+    expect(result[0].data.name).toEqual(name2);
+  });
+
+  test("find document", async () => {
+    const collection = "test find document";
+    await db.deleteCollection(collection);
+
+    await db.saveDocumentWithId(collection, id1, { name: name1 });
+
+    const result = await db.findDocument(collection, id1);
+
+    expect(result.name).toEqual(name1);
+  });
+});
