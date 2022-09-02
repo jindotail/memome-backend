ae28d2fd2d7b9f76c8bca701283c6f702fe8a860
mimseong <parkseongmin98@gmail.com>
2022-09-02 17:56:33 +0900

    Feat: makeIdx 함수 추가

--- src/common/random.ts
+++ src/common/random.ts
@@ -4,3 +4,14 @@ export const shuffle = (array: any[]) => {
     [array[i], array[j]] = [array[j], array[i]];
   }
 };
+
+export const makeIdx = (length: number) => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
