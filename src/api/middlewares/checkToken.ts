7d4a779e8a88ef4ee17e3aa2a2bc6cf4b23e3bc6
mimseong <parkseongmin98@gmail.com>
2023-05-07 14:22:02 +0900

    Refactor: 함수 재사용가능하도록 throw 위치 변경

--- src/api/middlewares/checkToken.ts
+++ src/api/middlewares/checkToken.ts
@@ -4,13 +4,11 @@ import { HttpStatusCode } from "../../common/http";
 import { verifyToken } from "../../common/jwt";
 import APIError from "../../errors/APIError";
 
-const checkTokenId = (id: string, tokenId: string) => {
-  if (id !== tokenId)
-    throw new APIError(
-      "verifyToken",
-      HttpStatusCode.UNAUTHORIZED,
-      "사용자의 토큰이 아닙니다"
-    );
+export const checkTokenId = (req: Request, apiType: string, tokenType: string) => {
+  const token = getTokenFromRequest(req, tokenType);
+  const varifiedToken = verifyToken(token) as JwtPayload;
+  const id = getIdFromRequest(req, apiType);
+  return id === varifiedToken.id;
 };
 
 // TODO - 이게 아닌 것 같은데 방법을 모르겠다
@@ -49,10 +47,12 @@ const getTokenFromRequest = (req: Request, tokenType: string) => {
 export const checkToken = (apiType: string, tokenType: string) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const token = getTokenFromRequest(req, tokenType);
-      const varifiedToken = verifyToken(token) as JwtPayload;
-      const id = getIdFromRequest(req, apiType);
-      checkTokenId(id, varifiedToken.id);
+      if (checkTokenId(req, apiType, tokenType))
+        throw new APIError(
+          "verifyToken",
+          HttpStatusCode.UNAUTHORIZED,
+          "사용자의 토큰이 아닙니다"
+        );
     } catch (e) {
       next(e);
       return;
